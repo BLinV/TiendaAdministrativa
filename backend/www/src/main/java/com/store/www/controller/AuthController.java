824fd13c1292de52476abacfdfcbc0ac18b1f0aa
BLinV <linares.98@hotmail.com>
2026-07-15 12:36:46 -0500

    refactor(backend): limpieza de import innecesario

--- backend/www/src/main/java/com/store/www/controller/AuthController.java
+++ backend/www/src/main/java/com/store/www/controller/AuthController.java
@@ -8,7 +8,6 @@ import com.store.www.service.AuthService;
 
 import jakarta.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
