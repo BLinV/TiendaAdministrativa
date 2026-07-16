ca5bf81b52eb14bc4141497bfbcf6843165fe9f8
BLinV <linares.98@hotmail.com>
2026-07-15 19:32:59 -0500

    feat(frontend): filtros de busqueda

--- frontend/www/src/app/features/productos/lista-productos/lista-productos.ts
+++ frontend/www/src/app/features/productos/lista-productos/lista-productos.ts
@@ -1,33 +1,45 @@
+import { CategoriaService } from './../../../core/services/categoria';
 import { Component, inject, signal, OnInit } from '@angular/core';
 import { ProductoService } from '../../../core/services/producto';
 import { Producto } from '../../../core/models/producto.model';
 import { CurrencyPipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { Categoria } from '../../../core/models/categoria.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-productos',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, FormsModule],
   templateUrl: './lista-productos.html',
   styleUrl: './lista-productos.scss'
 })
 export class ListaProductos implements OnInit {
   private productoService = inject(ProductoService);
+  private categoriaService = inject(CategoriaService);
   private router = inject(Router);
 
   productos = signal<Producto[]>([]);
   cargando = signal(false);
 
+  // Filtros
+  filtroCategoria = signal<number | null>(null);
+  filtroPrecioMax = signal<number | null>(null);
+  filtroNombre = signal<string>('');
+  categorias = signal<Categoria[]>([]);
+
   ngOnInit(): void {
+    this.categoriaService.listar().subscribe(c => this.categorias.set(c));  // para el dropdown
     this.cargarProductos();
   }
 
   cargarProductos(): void {
     this.cargando.set(true);
-    this.productoService.listar().subscribe({
-      next: (data) => {
-        this.productos.set(data);
-        this.cargando.set(false);
-      },
+    this.productoService.listar(
+      this.filtroCategoria() ?? undefined,
+      this.filtroPrecioMax() ?? undefined,
+      this.filtroNombre() || undefined
+    ).subscribe({
+      next: (data) => { this.productos.set(data); this.cargando.set(false); },
       error: () => this.cargando.set(false)
     });
   }
@@ -47,5 +59,12 @@ export class ListaProductos implements OnInit {
       next: () => this.cargarProductos(),   // recargar la lista tras borrar
       error: () => alert('No se pudo eliminar')
     });
+  }
+
+  limpiarFiltros(): void {
+    this.filtroCategoria.set(null);
+    this.filtroPrecioMax.set(null);
+    this.filtroNombre.set('');
+    this.cargarProductos();
   }
 }
